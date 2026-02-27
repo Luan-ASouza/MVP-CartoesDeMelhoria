4f8cb54af7b1d3c83bf04ec6f15c483fd3adbf7d
Luan Augusto de Souza <luande2010@gmail.com>
2026-02-26 23:39:14 -0300

    feat: adicionado condicional de somente o dono poder deletar

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -30,6 +30,7 @@ export function Card({ id, date, description, type, memberName, memberPhoto, onR
     <div className="bg-white rounded-3xl shadow-lg hover:shadow-2xl transition-all duration-300 overflow-hidden transform hover:-translate-y-1 flex flex-col h-full">
       {/* Header colorido com gradiente */}
       <div className={`bg-linear-to-br ${gradient} p-4 relative`}>
+        {currentUserName === memberName && (
         <div className="absolute top-3 right-3">
           <button
             onClick={() => onRemove(id)}
@@ -39,6 +40,7 @@ export function Card({ id, date, description, type, memberName, memberPhoto, onR
             <Trash2 size={16} />
           </button>
         </div>
+        )}
         
         <div className="flex items-center justify-between">
           <span className="inline-block px-3 py-1 bg-white/30 backdrop-blur-sm text-white rounded-full text-xs">
